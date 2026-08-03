6a4351d55209ae66f35f0b1f21fa67026fb1b068
Sudip Das <sudip.das@recievemoney.info>
2026-08-03 16:54:44 +0530

    login commit

---
+++ Login.py
@@ -0,0 +1,27 @@
+import time
+
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+
+    browser = p.chromium.launch(headless=False)
+
+    page = browser.new_page()
+
+    page.goto("https://org.dev.invorush.com/")
+    time.sleep(15)
+    ## Locate email id text field and fill the data :
+    page.get_by_placeholder("Enter your email").fill("newscenarictravelsllp@gmail.com")
+
+    time.sleep(5)
+
+    ## Locate password textfield and fill the data :
+
+    page.get_by_placeholder("Enter your password").fill("password2026")
+    time.sleep(5)
+
+    # locate the click button and click on 'sign in ' button :
+
+    page.locator("//span[text()='Sign In']").click()
+    time.sleep(15)
+    browser.close()
